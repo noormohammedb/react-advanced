3733ad12465585a0768ae8fa8f18c82777bba8ab
Noor Mohammed <nooru.mohad@gmail.com>
2021-03-04 21:06:36 +0530

    advanced useState with timeout

--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -1,7 +1,39 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateCounter = () => {
-  return <h2>useState counter example</h2>;
+   const [value, setValue] = useState(0);
+   const lateChange = (functionType) => {
+      setTimeout(() => {
+         //  setValue(value + functionType);
+         setValue((prev) => {
+            return prev + 1;
+         });
+      }, 500);
+   };
+   return (
+      <>
+         <h3>{value}</h3>
+         <button className="btn" onClick={() => setValue(value - 1)}>
+            decrement
+         </button>
+         <button className="btn" onClick={() => setValue(0)}>
+            reset
+         </button>
+         <button className="btn" onClick={() => setValue(value + 1)}>
+            increment
+         </button>
+         <div>
+            <h3 style={{ marginTop: "3rem" }}>Slow Buttons</h3>
+            <h3>{value}</h3>
+            <button className="btn" onClick={() => lateChange(-1)}>
+               decrement
+            </button>
+            <button className="btn" onClick={() => lateChange(+1)}>
+               increment
+            </button>
+         </div>
+      </>
+   );
 };
 
 export default UseStateCounter;
