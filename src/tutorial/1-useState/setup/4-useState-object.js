7c10a46a4de17086caec44384f1d717bc85a89bb
Noor Mohammed <nooru.mohad@gmail.com>
2021-03-04 18:25:16 +0530

    predding to hook

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+   const [personData, setPData] = useState({
+      name: "lorem ipsum",
+      age: 44,
+      address: "lorem ipsum dollor",
+   });
+   const clickEvent = () => {
+      console.log("Hellow");
+      setPData({ ...personData, address: "Hello World" });
+   };
+   return (
+      <>
+         <h3>{personData.name}</h3>
+         <h3>{personData.age}</h3>
+         <h3>{personData.address}</h3>
+         <button className="btn" onClick={clickEvent}>
+            Click Me
+         </button>
+      </>
+   );
 };
 
 export default UseStateObject;
