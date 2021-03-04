6141cd0ddd3ea2ccf37c17a0914bbda5b8be96d6
Noor Mohammed <nooru.mohad@gmail.com>
2021-03-04 15:17:58 +0530

    state change

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+   const [text, setText] = useState("Hello");
+   const changeText = () => {
+      console.log(text);
+      if (text === "Hello") {
+         setText("World");
+      } else {
+         setText("Hello");
+      }
+   };
+   return (
+      <>
+         <h2> {text} </h2>
+         <button className="btn" onClick={changeText}>
+            click me
+         </button>
+      </>
+   );
 };
 
 export default UseStateBasics;
